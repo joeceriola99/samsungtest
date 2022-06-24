250998985b60094b8a46ee7e4b3a02c0bb89386d
Joseph Ferdinand <joeceriola99@gmail.com>
2022-06-24 16:51:35 +0800

    placed it on form page

---
+++ pages/form.js
@@ -0,0 +1,53 @@
+import styles from "/./styles/Home.module.css";
+import { useForm } from "react-hook-form";
+
+export default function Home() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input {...register("name", { required: true })} placeholder="Name" />
+          {errors.name && <p> name is required.</p>}
+          <br /> <br />
+          <input {...register("age", { pattern: /\d+/ })} placeholder="Age" />
+          {errors.age && <p>Please enter number for age.</p>}
+          <br /> <br />
+          <input
+            value="male"
+            type="radio"
+            {...register("sex", { required: true })}
+          />
+          Male <br />
+          <input
+            value="female"
+            type="radio"
+            {...register("sex", { required: true })}
+          />
+          Female <br />
+          {errors.sex && <p>Please choose your Gender</p>}
+          <br />
+          <input type="checkbox" value="Java" {...register("progLanguage")} />
+          Java
+          <input
+            type="checkbox"
+            value="ReactJS"
+            {...register("progLanguage")}
+          />
+          ReactJS
+          <input type="checkbox" value="NextJS" {...register("progLanguage")} />
+          NextJS
+          <input type="checkbox" value="Python" {...register("progLanguage")} />
+          Python <br /> <br />
+          <input type="submit" />
+        </form>
+      </main>
+    </div>
+  );
+}
